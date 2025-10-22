4b0fb744c5e54ce2180e89bbe937822836f57d7b
AxelCornim <axelcornim@gmail.com>
2025-10-21 21:30:05 -0300

    adicao 2.0 da task contado termos PA usando While true:adicao run_1

--- Dev_solto/run_1.py
+++ Dev_solto/run_1.py
@@ -1239,3 +1239,26 @@ for user in range(user, 0, -1):
     
 print(f'= {fatorial}')"""
 
+"""while True:
+    try:
+        primeiro_termo = int(input('Primeiro termo: '))
+        razao = int(input('Razão: '))
+        break
+    except ValueError:
+        print('Entrada inválida!\nTente Novamente: ')   
+        
+termo = primeiro_termo
+cont = 1 
+total = 0
+mais = 10
+
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print(f'{termo} -> ', end='')
+        termo += razao
+        cont += 1
+    print('Pausa')
+    mais = int(input('Quantos termos quer mostrar a mais? '))
+    
+print(f'Progressão finalizada com {total} termos mostrados.')"""
